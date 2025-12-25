b59c1f47b2a579057c5f16b667fbe393b9ed6b96
ntuanh <ntuanh2705@gmail.com>
2025-12-25 23:51:52 +0700

    update : time layers measure

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ from src.Utils import get_output_sizes
 with open('cfg/config.yaml') as file:
     config = yaml.safe_load(file)
 
-# profiler = LayerProfiler(config, mode="time")
-# time_list = profiler.run()
-#
-# print(time_list)
+profiler = LayerProfiler(config, mode="time")
+time_list = profiler.run()
+
+print(time_list)
 
 profiler = LayerProfiler(config, mode="shape" , unit="MB")
 shape_list = profiler.run()
